59640be1b52c9cb350cfad3c618a8fd8d25ddb36
Chewy <chewybomber@gmail.com>
2016-10-30 10:43:51 +0100

    Initial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNKManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WwiseBank myBank = new WwiseBank(@"C:\Wooxy\extract\lol_game_client\DATA\Sounds\Wwise\SFX\Characters\Azir\Skins\Base\Azir_Base_SFX_audio.bnk");
+            myBank.Save(@"C:\Wooxy\extract\lol_game_client\DATA\Sounds\Wwise\SFX\Characters\Azir\Skins\Base\Azir_Base_SFX_audio2.bnk");
+        }
+    }
+}
---
+++ WwiseBank.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BNKManager
+{
+    public class WwiseBank
+    {
+        public List<BankSection> bankSections = new List<BankSection>();
+
+        public WwiseBank(string fileLocation)
+        {
+            using (BinaryReader br = new BinaryReader(File.Open(fileLocation, FileMode.Open)))
+            {
+                this.Read(br);
+            }
+        }
+
+        public void Save(string fileLocation)
+        {
+            using (BinaryWriter bw = new BinaryWriter(File.Open(fileLocation, FileMode.Create)))
+            {
+                this.Write(bw);
+            }
+        }
+
+        private void Read(BinaryReader br)
+        {
+            while (br.BaseStream.Position < br.BaseStream.Length)
+            {
+                string sectionName = Encoding.ASCII.GetString(br.ReadBytes(4));
+                uint sectionLength = br.ReadUInt32();
+                switch (sectionName)
+                {
+                    case "BKHD":
+                        this.bankSections.Add(new BKHDSection(br, sectionLength));
+                        break;
+                    case "HIRC":
+                        this.bankSections.Add(new HIRCSection(br));
+                        break;
+                    case "STID":
+                        this.bankSections.Add(new STIDSection(br));
+                        break;
+                    case "DIDX":
+                        this.bankSections.Add(new DIDXSection(br, sectionLength));
+                        break;
+                    case "DATA":
+                        this.bankSections.Add(new DATASection(br, sectionLength, (DIDXSection)this.GetSection("DIDX")));
+                        break;
+                    default:
+                        this.bankSections.Add(new BankSection(sectionName, br.ReadBytes((int)sectionLength)));
+                        break;
+                }
+            }
+        }
+
+        public BankSection GetSection(string sectionName)
+        {
+            return this.bankSections.Find(x => x.sectionName == sectionName);
+        }
+
+        private void Write(BinaryWriter bw)
+        {
+            foreach (BankSection bnkSection in this.bankSections)
+            {
+                bw.Write(bnkSection.GetBytes());
+            }
+        }
+    }
+}
